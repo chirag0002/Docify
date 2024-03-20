9c5dbd67174322881e2a7dbafccc70476aec6a72
Chirag Varshney <87241050+chirag0002@users.noreply.github.com>
2024-03-20 18:13:04 +0530

    Update api.ts

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://docify-sh78.onrender.com'
 
 export const API = axios.create({
     baseURL: BASE_URL,
